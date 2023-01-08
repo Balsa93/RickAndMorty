26f58f5cfecc8646152525846c6854053c4765dc
Balsa Komnenovic <komnenovicbalsa93@gmail.com>
2023-01-08 21:19:22 +0100

    Added models

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Balsa Komnenovic on 8.1.23..
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
