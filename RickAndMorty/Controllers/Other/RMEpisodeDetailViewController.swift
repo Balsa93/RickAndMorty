eefa36f7ffe637b98fb30fa1732b34d620c9ef97
Balsa Komnenovic <komnenovicbalsa93@gmail.com>
2023-01-13 15:02:07 +0100

    Added detail screen for episodes.

---
+++ RickAndMorty/Controllers/Other/RMEpisodeDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RMEpisodeDetailViewController.swift
+//  RickAndMorty
+//
+//  Created by Balsa Komnenovic on 13.1.23..
+//
+
+import UIKit
+
+/// VC to show details about single episode
+final class RMEpisodeDetailViewController: UIViewController {
+    private let url: URL?
+
+    //MARK: - Init
+    init(url: URL?) {
+        self.url = url
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Episode"
+        view.backgroundColor = .systemOrange
+    }
+}
