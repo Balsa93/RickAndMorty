621fc6b071c092fb4578387289b34abb4513b528
Balsa Komnenovic <komnenovicbalsa93@gmail.com>
2023-01-13 16:03:20 +0100

    Added API cache manager, episode view and view model.

--- RickAndMorty/APIClient/RMService.swift
+++ RickAndMorty/APIClient/RMService.swift
@@ -11,6 +11,7 @@ import Foundation
 final class RMService {
     /// Shared singleton instance
     static let shared = RMService()
+    private let cacheManager = RMAPICacheManager()
     
     /// Privatized constructor
     private init() {}
@@ -26,19 +27,31 @@ final class RMService {
     ///   - type: The type of object we expect to get back
     ///   - completion: Callback with data or error
     public func execute<T: Codable>(_ request: RMRequest, expecting type: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+        if let cachedData = cacheManager.cachedResponse(for: request.endpoint, url: request.url) {
+            print("Using cached API response")
+            do {
+                let result = try JSONDecoder().decode(type.self, from: cachedData)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+            return
+        }
+        
         guard let urlRequest = self.request(from: request) else {
             completion(.failure(RMServiceError.failedToCreateRequest))
             return
         }
-//        print("API Call: \(request.url?.absoluteString ?? "")")
-        let task = URLSession.shared.dataTask(with: urlRequest) { data, _, error in
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { [weak self] data, _, error in
             guard let data = data, error == nil else {
                 completion(.failure(error ?? RMServiceError.failedToGetData))
                 return
             }
-//            Decode response
+            //            Decode response
             do {
                 let result = try JSONDecoder().decode(type.self, from: data)
+                self?.cacheManager.setResponse(for: request.endpoint, url: request.url, data: data)
                 completion(.success(result))
             } catch {
                 completion(.failure(error))
---
+++ RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  RMEpisodeDetailView.swift
+//  RickAndMorty
+//
+//  Created by Balsa Komnenovic on 13.1.23..
+//
+
+import UIKit
+
+final class RMEpisodeDetailView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+}
