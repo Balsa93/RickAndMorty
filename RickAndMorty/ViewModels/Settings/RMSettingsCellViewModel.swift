954539ac6175f692888e8f77d3446366d50d3d01
Balsa Komnenovic <komnenovicbalsa93@gmail.com>
2023-01-21 17:50:39 +0100

    Started working on Settings VC, added Settings and settings cell view model

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsCellViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  RMSettingsCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Balsa Komnenovic on 21.1.23..
+//
+
+import UIKit
+
+struct RMSettingsCellViewModel: Identifiable, Hashable {
+    let id = UUID()
+    private let type: RMSettingsOption
+    
+    //MARK: - Init
+    init(type: RMSettingsOption) {
+        self.type = type
+    }
+    
+    //MARK: - Public
+    public var image: UIImage? {
+        return type.iconImage
+    }
+    public var title: String {
+        return type.displayTitle
+    }
+    public var iconContainerColor: UIColor {
+        return type.iconContainerColor
+    }
+}
---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Balsa Komnenovic on 21.1.23..
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+}
